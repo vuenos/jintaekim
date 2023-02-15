e8b189a6fea2816594a110b6eb234dcb9d32aefa
jintae kim <vuenos@naver.com>
2023-02-15 13:27:49 +0900

    Update profile info

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { UserOutlined, IdcardOutlined, ClockCircleOutlined, GithubOutlined } from "@ant-design/icons"
 
 const { Header, Content, Footer } = Layout;
-const { Text, Paragraph } = Typography;
+const { Title, Text, Paragraph } = Typography;
 
 const gnbItems: MenuProps['items'] = [
   {
@@ -293,8 +293,7 @@ const year2012 = [
     children: (
       <>
         <Paragraph>
-          아이웨어 브랜드 PROJEKTPRODUKT 온라인몰 구축<br />
-          카페24 솔루션을 이용하여 온라인몰 구축<br />
+          카페24 솔루션을 이용하여 아이웨어 브랜드 PROJEKTPRODUKT 온라인몰 구축<br />
           <a href="https://projektprodukt.co.kr/" target="_blank" rel="noreferrer">https://projektprodukt.co.kr</a>
         </Paragraph>
         <Paragraph>
@@ -464,7 +463,7 @@ export default function Home() {
               <Col md={12} sm={24}>
                 <Card bordered={false}>
                   <Meta
-                    avatar={<Avatar src="/IMG_0172.jpg" style={{ width: '120px' }} size={120} />}
+                    avatar={<Avatar src="/jtk.jpg" style={{ width: '120px' }} size={120} />}
                     title="김 진태"
                     description={
                       <Paragraph type="secondary">
@@ -515,7 +514,7 @@ export default function Home() {
             >
               <Col span={24} lg={12}>
                 <Card
-                  title="2021 ~ 2023"
+                  title={<Title level={4} style={{ margin: '0' }}>2021 ~ 2023 <Text type="secondary">(셀러허브, 팀장)</Text></Title>}
                   bordered={false}
                 >
                   <Alert
@@ -532,7 +531,7 @@ export default function Home() {
               </Col>
 
               <Col span={24} lg={12}>
-                <Card title="2018 ~ 2021" extra={<Text type="secondary">(주요 프로젝트만 기술함)</Text>} bordered={false}>
+                <Card title={<Title level={4} style={{ margin: '0' }}>2018 ~ 2021 <Text type="secondary">(프리렌서)</Text></Title>} extra={<Text type="secondary">(주요 프로젝트만 기술함)</Text>} bordered={false}>
                   <Alert
                     message="관공서, 금융, 항공사 프로젝트 진행함. 주로 Web, Mobile 반응형 UI 구현과 웹접근성 인증마크 획득"
                     type="info"
@@ -547,7 +546,7 @@ export default function Home() {
               </Col>
 
               <Col span={24} lg={12}>
-                <Card title="2012 ~ 2017" extra={<Text type="secondary">(주요 프로젝트만 기술함)</Text>} bordered={false}>
+                <Card title={<Title level={4} style={{ margin: '0' }}>2012 ~ 2017 <Text type="secondary">(코즈믹웹, Owner)</Text></Title>} extra={<Text type="secondary">(주요 프로젝트만 기술함)</Text>} bordered={false}>
                   <Alert
                     message="개인 창업 후 고도몰, 카페24 솔루션으로 쇼핑몰 구축과 워드프레스로 웹사이트 구축을 주로 함. Web + Mobile Responsive UI 를 주로 구현 함."
                     type="info"
@@ -562,7 +561,7 @@ export default function Home() {
               </Col>
 
               <Col span={24} lg={12}>
-                <Card title="2004 ~ 2011" extra={<Text type="secondary">(주요 프로젝트만 기술함)</Text>} bordered={false}>
+                <Card title={<Title level={4} style={{ margin: '0' }}>2004 ~ 2011 <Text type="secondary">(스타일**, 팀장)</Text></Title>} extra={<Text type="secondary">(주요 프로젝트만 기술함)</Text>} bordered={false}>
                   <Alert
                     message="관공서, 기업, 금융, 방송국 등의 웹접근성 인증마크 및 웹표준(웹 2.0) 개선 관련 컨설팅과 리팩토링 업무를 주로 함."
                     type="info"
